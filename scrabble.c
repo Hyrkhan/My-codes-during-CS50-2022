860bffa99c558b6374c5d8ba87f89da17f3deb97
Hyrkhan <109449003+Hyrkhan@users.noreply.github.com>
2022-09-10 15:04:26 +0800

    Create scrabble.c
    this is the code that cs50 wrote and I only added some things to create a functioning program. I wrote a code that can give the appropriate points for the word that the user entered. The program will compare two words entered by two different players and calculate the points of each word based on the scrabble word points, it will then announce the player with the most points as the winner.

---
+++ scrabble.c
@@ -0,0 +1,59 @@
+#include <ctype.h>
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+// Points assigned to each letter of the alphabet
+int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+
+int compute_score(string word);
+
+int main(void)
+{
+    // Get input words from both players
+    string word1 = get_string("Player 1: ");
+    string word2 = get_string("Player 2: ");
+
+    // Score both words
+    int score1 = compute_score(word1);
+    int score2 = compute_score(word2);
+
+    // TODO: Print the winner
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 == score2)
+    {
+        printf("It's a tie!\n");
+    }
+    else
+    {
+        printf("Player 2 wins!\n");
+    }
+}
+
+int compute_score(string word)
+{
+    //create a string for abcds
+    //create for loop to loop string word
+    //loop the alphabet array inside the string word loop
+    //lower case the letters inside the string word while looping
+    //if abcds[i] == string word then add points according to POINTS[i]
+    // TODO: Compute and return score for string
+    char ch;
+    int point = 0;
+    char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    for (int i = 0; i < strlen(word); i++)
+    {
+        ch = tolower(word[i]);
+        for (int j = 0; j < 26 ; j++)
+        {
+            if (ch == alphabet[j])
+            {
+                point = point + POINTS[j];
+            }
+        }
+    }
+    return point;
+}
